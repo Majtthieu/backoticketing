1869e837bf8f2c94c6d46e3131f9952a90e4e5db
majtthieu <matthieu.jankowiak@yahoo.fr>
2024-11-15 16:39:38 +0100

    Initial commit

---
+++ src/main/java/fr/oticketing/OTicketingApplication.java
@@ -0,0 +1,11 @@
+package fr.oticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OTicketingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OTicketingApplication.class, args);
+    }
+}
---
+++ src/main/java/fr/oticketing/model/User.java
@@ -0,0 +1,36 @@
+package fr.oticketing.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String username;
+    
+    @Column(nullable = false)
+    private String firstName;
+    
+    @Column(nullable = false)
+    private String lastName;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    @Column(nullable = false)
+    private String secretKey;
+    
+    private boolean enabled = true;
+    
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+}
